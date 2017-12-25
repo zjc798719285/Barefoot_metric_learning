086e114e3dfc30db9fc35f4ccf6d61378932551f
zjc798719285 <798719285@qq.com>
2017-12-25 11:34:18 +0800

    Add a new function getfilelist and delete mkdir1 readlist

--- test_folder/config.py
+++ test_folder/config.py
@@ -5,11 +5,8 @@ FLAGS = tf.app.flags.FLAGS
 #   parameters setting  #
 #########################
 
-tf.app.flags.DEFINE_string('train', 'E:\PROJECT\Fisher_loss\\feature\\footnet_v8_256_metric_v4\img', 'train path')
-tf.app.flags.DEFINE_string('test', '\\path', 'test path')
-tf.app.flags.DEFINE_string('output', 'E:\PROJECT\Fisher_loss\\feature\\footnet_v8_256_metric_v4\output', 'output path')
-tf.app.flags.DEFINE_string('output_train', 'E:\PROJECT\Fisher_loss\\feature\\footnet_v8_256_metric_v4\output', 'output_train dir')
-tf.app.flags.DEFINE_string('output_test', '\\path', 'output_test dir')
+tf.app.flags.DEFINE_string('input_dir', 'E:\PROJECT\Fisher_loss\\feature\\footnet_v8_256_metric_v4\img', 'input_dir')
+tf.app.flags.DEFINE_string('output_dir', 'E:\PROJECT\Fisher_loss\\feature\\footnet_v8_256_metric_v4\output', 'output_dir')
 tf.app.flags.DEFINE_string('checkpoint', 'E:\PROJECT\capsule_net\Barefoot_metric_learning\\test_folder\checkpoint\\fisher_loss2378.ckpt', 'model path')
 
 tf.app.flags.DEFINE_string('model', 'footnet_v9', 'model_file_name')
