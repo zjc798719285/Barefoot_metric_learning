b32d37035c2bfcc2d66d6e02dbc174b5f9b65842
zjc798719285 <798719285@qq.com>
2017-12-25 11:50:10 +0800

    mkdir getfilelist have been modified

--- test_folder/utils.py
+++ test_folder/utils.py
@@ -5,43 +5,25 @@ import numpy as np
 from config import FLAGS
 import scipy.io as sio
 from footnet_v9 import my_alex
-def readlist(path, output):
-    file_list = []
-    output_list = []
-    folder = os.listdir(path)
-    for folder_i in folder:
-        folder_path = os.path.join(path, folder_i)
-        output_path = os.path.join(output, folder_i)
-        filename = os.listdir(folder_path)
-        for filename_i in filename:
-            file_path = os.path.join(folder_path, filename_i)
-            out_file_path = os.path.join(output_path, filename_i)
-            file_list.append(file_path)
-            output_list.append(out_file_path)
-    return file_list, output_list
 def getfilelist(input_dir, file_path, output_dir, output_path):
     for sub_path in os.listdir(input_dir):
         filename = os.path.join(input_dir, sub_path)
         outfile = os.path.join(output_dir, sub_path)
         if os.path.isdir(filename):
-            getfilelist(filename, file_path, output_dir, output_path)
+            getfilelist(filename, file_path, outfile, output_path)
         else:
             file_path.append(filename)
             output_path.append(outfile)
     return
-def mkdir1(path, output):
-   folder = os.listdir(path)
-   for folder_i in folder:
-     folder_path_out = os.path.join(output, folder_i)
-     if not os.path.exists(folder_path_out):
-        os.mkdir(folder_path_out)
 def mkdir(path, output):
     for sub_path in os.listdir(path):
         filepath = os.path.join(path, sub_path)
         outpath = os.path.join(output, sub_path)
         if os.path.isdir(filepath):
-            os.mkdir(outpath)
+            if not os.path.exists(outpath):
+                os.mkdir(outpath)
             mkdir(filepath, outpath)
+
 def img2vec(filelist, output_list):
    x = tf.placeholder(tf.float32, [1, FLAGS.height, FLAGS.width, FLAGS.channel])
    model = my_alex(x, 1, 1000)  #下一版本要改进，用参数搜集
